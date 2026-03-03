180bdb600457bf07a661bc997a0487eb9e0984b7
Ken Johnson <kenmjohnson03@vt.edu>
2026-03-03 14:59:21 -0500

    Initial map functionality push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 import './App.css'
+
+
+function DisplayMap()
+{
+const example_banks = [
+    { position: [38.9072, -77.0369], name: "Washington DC" },
+    { position: [39.0840, -77.6413], name: "Loudoun County" },
+    { position: [38.8816, -77.0910], name: "Arlington" },
+  ]
+  return(
+    <MapContainer center={example_banks[0].position} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}
+    whenReady={(map) => {
+    const bounds = example_banks.map(loc => loc.position)
+    map.target.fitBounds(bounds, { padding: [10, 10] })
+    }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {example_banks.map((loc, index) => (
+          <Marker key={index} position={loc.position}>
+          <Popup>{loc.name}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ height: '100%', width: '100%' }}>
+      <div style = {{height: '50%', width: '50%', marginLeft: '10%', marginRight: '10%'}}>
+        <DisplayMap/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
